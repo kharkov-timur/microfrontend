86d6f7084ace33b8a51488d784ee6ca120961277
kharkov-timur <t1i9m8u8r2202>
2022-04-28 11:02:18 +0300

    Implement AuthApp

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -1,21 +1,22 @@
+import { mount } from 'auth/AuthApp';
 import React, { useRef, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import { mount } from 'marketing/MarketingApp';
 
-export default () => {
+export default ({ onSignIn }) => {
     const ref = useRef(null);
     const history = useHistory();
 
     useEffect(() => {
         const { onParentNavigate } = mount(ref.current, {
             initialPath: history.location.pathname,
-            onNavigate: ({ pathname: nextPathName }) => {
+            onNavigate: ({ pathname: nextPathname }) => {
                 const { pathname } = history.location;
 
-                if (pathname !== nextPathName) {
-                    history.push(nextPathName);
+                if (pathname !== nextPathname) {
+                    history.push(nextPathname);
                 }
             },
+            onSignIn,
         });
 
         history.listen(onParentNavigate);
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { useHistory } from 'react-router-dom';
 import { createMemoryHistory, createBrowserHistory } from 'history';
 
 import { App } from './App';
 
-const mount = (el, { onNavigate, defaultHistory }) => {
-    const history = defaultHistory || createMemoryHistory();
+export const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+    const history =
+        defaultHistory ||
+        createMemoryHistory({
+            initialEntries: [initialPath],
+        });
 
     if (onNavigate) {
         history.listen(onNavigate);
@@ -32,5 +35,3 @@ if (process.env.NODE_ENV === 'development') {
         mount(devRoot, { defaultHistory: createBrowserHistory() });
     }
 }
-
-export { mount };
